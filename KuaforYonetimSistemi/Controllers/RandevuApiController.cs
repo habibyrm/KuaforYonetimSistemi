d4410d8b706548648efcef3f05f2daaca579e167
habibyrm <bayramhabibe2003@gmail.com>
2024-12-20 04:38:55 +0300

    Sayfa üzerinde yetkilendirme ve sayfa tasarımı

---
+++ KuaforYonetimSistemi/Controllers/RandevuApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KuaforYonetimSistemi.Controllers
+{
+    public class RandevuApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
